daef4593901b73fcff8d04be6a813eddad3fd7d7
Kevin Tan <thektan@gmail.com>
2015-08-06 15:55:28 -0700

    Decreased height of header and changed typeface

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-$(document).ready(function () 
+$(document).ready(function ()
 {
     // Variables for when header bar will become fixed.
     var elementPosition = $('.title-container').offset();
     var headerBackgroundPosition = $('#full-page-container').offset();
-    
+
     /**
      * Fixed header bar.
      */
-    $(window).scroll(function () 
+    $(window).scroll(function ()
     {
         // Fix position for "Kevin Tan."
         if ($(window).scrollTop() + 10 > elementPosition.top) {
@@ -16,7 +16,7 @@ $(document).ready(function ()
         else {
             $('#main-title').css('position', 'static');
         }
-        
+
         // Fix position for contact links.
         if ($(window).scrollTop() + 20 > elementPosition.top){
             $('#main-links').css('position', 'fixed').css('top', '20px');
@@ -24,11 +24,11 @@ $(document).ready(function ()
         else {
             $('#main-links').css('position', 'static');
         }
-        
+
         // Fix position of the header.
         if ($(window).scrollTop() + 150 > headerBackgroundPosition.top) {
-            $('#header-container').css('position', 'fixed').css('margin-top', '-510px');
-            $('#full-page-container').css('padding-top', '500px'); // To adjust for displacement of header.
+            $('#header-container').css('position', 'fixed').css('margin-top', '-260px');
+            $('#full-page-container').css('padding-top', '250px'); // To adjust for displacement of header.
         }
         else {
             $('#header-container').css('position', 'static').css('margin-top', '0');
